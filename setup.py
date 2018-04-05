6c7e9e93406d5c21d22dd03c785d00527d342757
Erik Fasterius <erikfas@kth.se>
2018-04-05 12:22:06 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='clustervariants',
+      version='0.1.0',
+      description='Cluster single nucleotide variants from HTS data',
+      author='Erik Fasterius',
+      author_email='erikfas@kth.se',
+      license='MIT',
+      packages=['clustervariants'],
+      zip_safe=False)
