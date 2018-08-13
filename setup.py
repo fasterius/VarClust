3b65d2c79c5900a9a1b74f162b9aa911d5cc7637
Erik Fasterius <erikfas@kth.se>
2018-08-13 10:09:22 +0200

    Increment version to 0.2.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='VarClust',
-      version='0.2.0',
+      version='0.2.1',
       description='Variant clustering of high throughput sequencing data',
       author='Erik Fasterius',
       author_email='erikfas@kth.se',
